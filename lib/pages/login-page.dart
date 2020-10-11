d6b72916257d102e97435d1e709f73fb3f0fa9e3
unknown <bfranco@unab.edu.co>
2020-10-11 10:19:35 -0500

    segundo commit

--- lib/pages/login-page.dart
+++ lib/pages/login-page.dart
@@ -1,22 +1,28 @@
 import 'package:finapp/login-state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 class LoginPage extends StatelessWidget {
-
-  
   @override
   Widget build(BuildContext context) {
-    final authservices =  Provider.of<LoginState>(context);
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('Entre lk'),
-          onPressed: () async{
-            await authservices.googleSignIn();
-          })
+    final authservices = Provider.of<LoginState>(context);
+
+    return Stack(
+      children: <Widget>[
+        FlareActor("assets/login.flr",
+            fit: BoxFit.contain, animation: "success"),
+
+        ////////////////////
+        Scaffold(
+          body: Center(
+              child: RaisedButton(
+                  child: Text('Entre lk'),
+                  onPressed: () async {
+                    await authservices.googleSignIn();
+                  })),
         ),
-      );
-    
+      ],
+    );
   }
 }
